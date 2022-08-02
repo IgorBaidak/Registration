c3c8ea7011743328fa11824208409185f94ec833
IgorBaidak <igorbaidak@gmail.com>
2022-08-02 21:02:29 +0300

    work Build 1

--- Registration/Registration/Controllers/CreateAccountViewController.swift
+++ Registration/Registration/Controllers/CreateAccountViewController.swift
@@ -9,12 +9,76 @@ import UIKit
 
 class CreateAccountViewController: UIViewController {
 
+    // MARK: - @IBOutlet
+    
+    ///email
+    @IBOutlet private weak var emailTF: UITextField!
+    @IBOutlet private weak var errorEmailLbl: UILabel!
+    ///name
+    @IBOutlet private weak var nameTF: UITextField!
+    ///password
+    @IBOutlet private weak var passwordTF: UITextField!
+    @IBOutlet private weak var errorPaswordLbl: UILabel!
+    @IBOutlet private weak var confirmPasswordTF: UITextField!
+    @IBOutlet private weak var errorConformPasswordLbl: UILabel!
+    ///passwordIndicator
+    @IBOutlet private var strongPasswordIndicator: [UIView]!
+    ///signUp Button
+    @IBOutlet private weak var signUpButton: UIButton!
+    
+    // MARK: - viewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        hideKeyboardWhenTappedAround()
     }
     
+    // MARK: - Properties
+    
+    private var validEmail = false
+    private var validConfirmPassword = false
+    private var strongPassword: PasswordStrength = .veryWeak
+    
+    // MARK: - @IBAction
+    
+    @IBAction func emailAction(_ sender: UITextField) {
+        if let email = sender.text,
+            !email.isEmpty,
+            VerificationService.isValidEmail(email: email){
+            validEmail = true
+        } else {
+            validEmail = false
+        }
+        errorEmailLbl.isHidden = validEmail
+    }
+    
+    @IBAction func nameAction(_ sender: UITextField) {
+        
+    }
+    
+    @IBAction func passwordAction(_ sender: UITextField) {
+        if let password = sender.text,
+           !password.isEmpty {
+           strongPassword = VerificationService.isValidPassword(pass: password)
+        }
+        errorPaswordLbl.isHidden = strongPassword == .veryWeak
+        }
+        
+    
+    @IBAction func confirmPasswordAction(_ sender: UITextField) {
+        
+    }
+    
+    @IBAction func signInButton() {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBAction func signUpButton(_ sender: UIButton) {
+        
+    }
+    
+    
+}
 
     /*
     // MARK: - Navigation
@@ -26,4 +90,4 @@ class CreateAccountViewController: UIViewController {
     }
     */
 
-}
+
---
+++ Registration/Registration/Service/Extension/UIViewControllerExtension.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewControllerExtension.swift
+//  Registration
+//
+//  Created by Igor Baidak on 2.08.22.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+    let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+    tap.cancelsTouchesInView = false
+    view.addGestureRecognizer(tap)
+    }
+
+@objc func dismissKeyboard() {
+    view.endEditing(true)
+    }
+    
+}
+
